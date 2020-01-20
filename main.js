35b30c460302febf3c8978f9a36e3aea65d8f1c1
giftyinyang <caomiehinyang@gmail.com>
2020-01-20 16:18:29 +0100

    new-commit-for-game

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        
-    }
+        display: true,
+        displayOne: false,
+        displayTwo: false,
+        number: Math.round(Math.random()*10),
+        numberTwo: Math.round(Math.random()*10),
+        myScore: 100,
+        monsterScorer: 100,
+        hits: '',
+        monster: '',
+
+     
+    },
 })
