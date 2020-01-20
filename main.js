122223d4447b66e85b1ce1bd228cbce0849a9876
giftyinyang <caomiehinyang@gmail.com>
2020-01-20 16:25:43 +0100

    lastest-game

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ new Vue({
         number: Math.round(Math.random()*10),
         numberTwo: Math.round(Math.random()*10),
         myScore: 100,
-        monsterScorer: 100,
+        monsterScore: 100,
         hits: '',
         monster: '',
 
      
     },
+    methods: {
+        changeDisplay() {
+            this.display = false;
+            this.displayOne = true;
+            this.displayTwo = false;
+            this.myScore = 100;
+            this.monsterScore = 100;
+        },
+    }
 })
